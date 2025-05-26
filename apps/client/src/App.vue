c663bcf304ad951876b0133983d1d4a4fec2c2c7
thicianny.marques <thicianny2@gmail.com>
2025-05-26 17:46:06 -0300

    Corrige rastreamento de arquivos ignorados

---
+++ apps/client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold">Teste API</h1>
+    <p class="mt-2">{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('Carregando...')
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:3000/api/hello')
+  message.value = res.data.message
+})
+</script>
